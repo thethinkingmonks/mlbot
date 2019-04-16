642319635c94ccfc867f4a28dd1212a0bb36963e
Rangarajan R <thethinkingmonks@gmail.com>
2019-04-16 22:44:21 +0530

    basic logger

---
+++ project/mlbot.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from log import *
+import preprocessing
+
+
+class mlbot:
+    def __init__(self):        
+
+        logger.info("Initialize ML Bot ...")
+
+def main():
+    bot = mlbot()
+    preprocessing.preprocessing()
+  
+if __name__ == '__main__':
+    main()
---
+++ project/preprocessing.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from log import *
+
+
+class preprocessing:
+    def __init__(self):
+        logger.info("Initialize preprocessing ...")    
