a30f3f4a417f4b8a4438850ce70667e5363177e8
thethinkingmonks <41247169+thethinkingmonks@users.noreply.github.com>
2019-06-11 17:22:14 +0530

    Update botdataloader.py

--- project/botdataloader.py
+++ project/botdataloader.py
@@ -7,14 +7,17 @@ class botdataloader:
     def __init__(self, bot):
         self.logger = bot["logger"]
         self.config = bot["config"]
-        self.logger.info("Initialize Data Loader ...")
+        self.logger.info("botdataloader - init ...")
  
     def load_data(self):
+        self.logger.info("botdataloader - load_data ...")
         config = self.config.config
-        self.df = pd.read_csv(config["input_file"],sep=config['separator'])        
+        self.df = pd.read_csv(config["input_file"],sep=config['separator'])
     
     def get_dataframe(self):
+        self.logger.info("botdataloader - get_dataframe ...")
         return self.df
     
-    def start_flow(self):        
+    def start_flow(self):
+        self.logger.info("botdataloader - start_flow ...")
         self.load_data()        
